4805457741ce8442c452ed4cf2344713a9b05272
Raphael Cerqueira <rcerqueira@vr.com.br>
2021-01-24 13:56:15 -0300

    Initial Commit

---
+++ MealApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MealApp
+//
+//  Created by Raphael Cerqueira on 24/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MealApp/MealAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealAppApp.swift
+//  MealApp
+//
+//  Created by Raphael Cerqueira on 24/01/21.
+//
+
+import SwiftUI
+
+@main
+struct MealAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
